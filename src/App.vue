48f0fb04c837adbecebc98a2fa5a86e274c6359a
HeinStrauss <heinstrauss56@outlook.com>
2022-03-02 12:18:46 +0200

    toDoApp Complete

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,79 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>To do list</h1>
+    <form @submit.prevent="addNewToDo">
+      <label>To Do:</label>
+      <input  type="text" v-model="newToDo"/>
+      <button>Add New ToDo</button>
+    </form>
+<br>
+  <h1>To pack list</h1>
+    <form>
+  <label>To Pack:</label>
+  <input  type="text" v-model="newToPack"/>
+  <button @click.prevent="addNewToPack(newToPack)">Pack new item</button>
+    </form>
+    <div v-for="(todo, index) in toDoList" :key="index">
+      <h3 :class="{done: todo.done}" @click="toggleDone(todo)">{{todo.content}}</h3>
+      <button @click="deleteTodo(todo, index)">Delete</button>
+    </div>
+<br>
+  <button @click="markAllDone">Mark All Done</button>
+  <button @click="unmarkAll">Unmark All </button>
+<br>
+  <button @click="deleteAll">Delete All</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { ref } from 'vue';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  setup(){
+    //variables
+    const newToPack = []
+    const newToDo = ref('');
+    const toDoList = ref([]);
+    //functions
+    function addNewToPack(item){
+      console.log('You need to pack in ' + item)
+    }
+    function addNewToDo() {
+      console.log('You need to ' + newToDo.value );
+      toDoList.value.push({
+        done:false,
+        content: newToDo.value,
+      });
+      newToDo.value="";
+    }
+   function toggleDone(todo){
+     todo.done = !todo.done
+   }
+   function deleteTodo(todo, index){
+     toDoList.value.splice(index, 1);
+     console.log("you deleted an item");
+   }
+   function markAllDone(){
+     toDoList.value.forEach ((todo) => todo.done=true);
+   }
+   function unmarkAll(){
+     toDoList.value.forEach ((todo) => todo.done=false);
+   }
+   function deleteAll(){
+     toDoList.value = [];
+   }
+    //returns
+    return {
+      unmarkAll,
+      deleteAll,
+      markAllDone,
+      deleteTodo,
+      toggleDone,
+      addNewToPack,
+      addNewToDo,
+      newToDo,
+      toDoList,
+      newToPack,
+    };
+  },
 }
 </script>
 
@@ -22,5 +85,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.done{
+  text-decoration: line-through;
 }
 </style>
